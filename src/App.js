230d0b5eece6e60b985ba0ff83390690a4b96ae6
Marcel Ramos <marceldavid@hotmail.com>
2025-04-10 15:22:19 -0400

    The components were set

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+// Shortcut to the emoji list Wid + .
+function Logo() {
+  return <h1>🛣️ Travel App 🏖️</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip? ✈️ </h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+//<em> = enfasize
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>🧳You have X items in your list and already packed x </em>
+    </footer>
+  );
+}
