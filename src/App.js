a9072ea137b686251d17b6432b5abeb17c6f2005
Marcel Ramos <marceldavid@hotmail.com>
2025-04-10 20:07:15 -0400

    building a form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+/****************************************************************** */
 export default function App() {
   return (
     <div className="app">
@@ -8,24 +14,69 @@ export default function App() {
     </div>
   );
 }
-
+/******************************************************************** */
 // Shortcut to the emoji list Wid + .
 function Logo() {
   return <h1>🛣️ Travel App 🏖️</h1>;
 }
-
+/******************************************************************** */
 function Form() {
+  //When you click on add button, tha page auto-reload. To avoid that , use e.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="add-form">
+    <form
+      className="add-form"
+      onSubmit={handleSubmit}>
       <h3>What do you need for your trip? ✈️ </h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option
+            value={num}
+            key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+/******************************************************************** */
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((itemArr) => (
+          <Item
+            itemProp={itemArr}
+            key={itemArr.id}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
-
-function PackingList() {
-  return <div className="list">LIST</div>;
+/******************************************************************** */
+//Using conditional operator to apply some style. If the item is packed(true),
+// then it will appear crossed out
+function Item({ itemProp }) {
+  return (
+    <li>
+      <span style={itemProp.packed ? { textDecoration: "line-through" } : {}}>
+        {itemProp.quantity + " "}
+        {itemProp.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
-
+/******************************************************************** */
 //<em> = enfasize
 function Stats() {
   return (
