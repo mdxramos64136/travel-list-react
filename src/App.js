005901e427defdb5ddc0fe33d7216d5a462f2c68
Marcel Ramos <marceldavid@hotmail.com>
2025-04-16 19:04:30 -0400

    sort features implemented on packing list

--- src/App.js
+++ src/App.js
@@ -94,10 +94,24 @@ function Form({ onAddItemProp }) {
 }
 /******************************************************************** */
 function PackingList({ listItemsProp, onDeleteItemProp, onToggleItemProp }) {
+  //create a state to deal with sort function
+  const [sortBy, setSortBy] = useState("input");
+
+  //receives the new sorted array depending on the sort criteria:
+  let sortedItemsArr;
+
+  // a is an (actual) object of the array. b is the next one
+  if (sortBy === "description")
+    sortedItemsArr = listItemsProp.slice().sort((a, b) => a.description.localeCompare(b.description));
+
+  //id has the date/ time. Cronological order of input
+  if (sortBy === "input") sortedItemsArr = listItemsProp.slice().sort((a, b) => a.id - b.id);
+
+  if (sortBy === "packed") sortedItemsArr = listItemsProp.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {listItemsProp.map((itemArr) => (
+        {sortedItemsArr.map((itemArr) => (
           <Item
             itemProp={itemArr}
             key={itemArr.id}
@@ -106,6 +120,15 @@ function PackingList({ listItemsProp, onDeleteItemProp, onToggleItemProp }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by pack status</option>
+        </select>
+      </div>
     </div>
   );
 }
